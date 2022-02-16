9198fbb7a2f6b14392c66c68c2d1b712c17e2948
Jose A Hernando <hernando.joseangel@gmail.com>
2022-02-16 13:58:51 +0100

    fanal

--- ana/fanal.py
+++ ana/fanal.py
@@ -93,16 +93,31 @@ def selection(df, varnames, varranges, full_output = False):
     out = sels if full_output else sel
     return out
 
-def selection_analysis(xdf,  
-                       xroi   = eroi, 
-                       eblob2 = eblob2):
-    sel = (xdf.num_tracks == 1) & (xdf.E >= xroi[0]) & \
-        (xdf.E < xroi[1]) & (xdf.blob2_E > eblob2)
-    return sel
+#def selection_analysis(xdf,  
+#                       xroi   = eroi, 
+#                       eblob2 = eblob2):
+#    sel = (xdf.num_tracks == 1) & (xdf.E >= xroi[0]) & \
+#        (xdf.E < xroi[1]) & (xdf.blob2_E > eblob2)
+#    return sel
 
 
 
 def selection_blind(df, eroi = eroi, eblob2 = eblob2):
+    """
+    
+    returns the blind selection
+
+    Parameters
+    ----------
+    df     : DataFrame, data
+    eroi   : tuple(float, float), Energy Track0 blind range
+    eblob2 : tuple(float, float), Energy range of the 2nd blob
+
+    Returns
+    -------
+    sel    : np.array(bool), selection of blind events
+
+    """
     sel0 = (df.track0_E >= eroi[0]) &  (df.track0_E < eroi[1])
     sel1 = (df.blob2_E  > eblob2)
     sel  = np.logical_or(sel0, sel1)
@@ -124,15 +139,24 @@ def selection_blind(df, eroi = eroi, eblob2 = eblob2):
 #     return bmcs
 
 
-def generate_mc_experiment(mcs, nevents):
+def generate_mc_experiment(mcs, nevts, unevts = None):
     """ generate a MC experiment with the mcs samples, mcs, and the events in each sample, nevents
     inputs  :
         mcs     : tuple(DF), DFs with the mc samples
         nevents : tuple(int), number of events to select in each sample
+        unevts  : tuple(float) or None, constrain the input events with a gaussian of sigma unevts
     returns :
         mc      : DF, DF with the mix of number event of events, nevents, of the initial samples, mcs
     """
-    xmcs = [mc.sample(n = int(ni)) for mc, ni in zip(mcs, nevents)] # sample ni events of mc-sample mci
+    unevts = len(nevts) * [None,] if unevts == None else unevts
+    #print(unevts)
+    #nns  = [stats.poisson.rvs(ni, size = 1)[0] for ni in nevents]
+    def _ni(ni, un):
+        nk = ni if un == None else stats.norm.rvs(ni, un, size = 1)[0]
+        return stats.poisson.rvs(nk, size = 1)[0]
+    nns  = [_ni(ni, un) for ni, un in zip(nevts, unevts)]
+    #print(nns)
+    xmcs = [mc.sample(n = ni) for mc, ni in zip(mcs, nns)] # sample ni events of mc-sample mci
     mc   = pd.concat(xmcs) # concatenate all background to generate a mc sample that mimics the data-blind sample
     return mc
 
@@ -201,10 +225,11 @@ def plot_fit_ell(x,
                  plot_residuals = True):
     """ plot the data x, and superimposed the pdf with parameters (par)
     inputs:
-        x   : np.array(float), data to plot
-        par : tuple(float), parameters of the pdf
-        pdf : function, pdf(x, *par), returns the pdf values of the distribution along x
-        parnames: tuple(str), list of the parameters names to write them in the plot legend
+        x    : np.array(float), data to plot
+        par  : tuple(float), parameters of the pdf
+        pdf  : function, pdf(x, *par), the pdf values of the distribution along x
+        pdfs : tuple(pdfs), the mc pdfs for each sample
+        parnames: tuple(str), list of the parameters (and samples) for the legend
     """
 
     subplot = pltext.canvas(1, 1, 8, 10)
@@ -214,8 +239,8 @@ def plot_fit_ell(x,
     centers = 0.5 * (edges[1:] + edges[:-1])
     ecounts = np.sqrt(counts)
     sel     = ecounts > 0
-    nn     = np.sum(par)
-    factor = nn * (centers[1] - centers[0])
+    nn      = np.sum(par)
+    factor  = nn * (centers[1] - centers[0])
     plt.errorbar(centers[sel], counts[sel], yerr = ecounts[sel], 
                  marker = 'o', ls = '', label = 'data')
 
@@ -253,6 +278,28 @@ def ana_samples(data,
                 varranges = varranges[:-1],
                 mc_level   = -1,
                 verbose    = True):
+    """
+    
+    Return the sample to analysis after a selection in the variables *varnames* 
+    in ranges *varranges*
+
+    Parameters
+    ----------
+    data      : DataFrame, data
+    mcs       : tuple(DataFrame), mc data frames for the different samples
+    varnames  : tuple(float), list of the variables names of the selection
+    varranges : tuple( (float, float)), list of the variable ranges of the selection
+    mc_level  : int, reduce the selection for mc samples to get more stats for the pdfs
+                a given level, default = -1
+    verbose   : bool, print into
+
+    Returns
+    -------
+    anadata   : DataFrame, the selected data events
+    anamcs    : tuple(DataFrame), the selected mc events (to get the pdfs)
+    effs      : tuple(float), selection efficiencies in the different samples
+
+    """
     
     effs    = [efficiencies(mc, varnames, varranges)[0][-1]  for mc in mcs]
     anamcs  = [mc[selection(mc, varnames[:mc_level], varranges[:mc_level])] for mc in mcs]
@@ -310,6 +357,33 @@ def ana_experiment(data,
                    varrange  = erange,
                    bins      = 150,
                    verbose   = True): 
+    """
+
+    perform the analysis
+
+    Parameters
+    ----------
+    data   : DataFrame, data
+    mcs    : tuple(DataFrame), mc data (bb, Bi, Tl)
+    nevts  : tuple(float), number of expected total events of each sample
+    unevts : tuple(float), optional, uncertainties of the expected total events in each sample
+        if provided, the fit is constrained
+    varnames  : tuple(strig), list of the variables of the selection
+    varranges : tuple( (float, float)), list with the selection variable ranges
+    mc_level  : int, optional, the mc samples for the pdfs use a 'downscaled' selection 
+    varname   : float, variable of the fit. The default is 'E'.
+    varrange  : (float, float), range of the fit. The default is erange.
+    bins      : int, optional, number of bins. The default is 150.
+    verbose   : bool, optional, print out and plots. The default is True.
+
+    Returns
+    -------
+    result    : object, result of the fit with the LL, and the estimated parameters
+    enes      : np.array(float), data values entered into the fit
+    ell       : object, extended LL fit object, access to the loglike
+    pdfs      : tuple(object), pdf objects associated to each sample
+
+    """
     
     anadata, anamcs, effs = ana_samples(data, mcs, varnames, varranges, 
                                         mc_level = mc_level, verbose = verbose)
@@ -330,91 +404,6 @@ def ana_experiment(data,
     
     return result, enes, ell, pdfs
     
-
-def __ana_experiment(data,
-                   mcs,
-                   nevts,
-                   unevts    = None,
-                   varnames  = varnames[:-1],
-                   varranges = varranges[:-1],
-                   mc_level  = -1,
-                   varname   = 'E',
-                   bins      = 100,
-                   varrange  = erange,
-                   verbose   = True): 
-
-
-    # estimate number of events and uncertainties
-    sels  = [selection(mc, varnames, varranges) for mc in mcs]
-    #print('sizes mc : ', [np.sum(sel) for sel in sels])                         
-    effs  = [ut.efficiency(sel)[0] for sel in sels]
-    if (verbose):
-        print('selections       : ', varnames)
-        print('selections range : ', varranges)
-        print('efficiencies    : ' , effs)
-    
-    nns   = [eff * n  for eff, n  in zip(effs, nevts )]
-    def _us():
-        unns  = [eff * un for eff, un in zip(effs, unevts)]
-        unns[0] = False # No constrain in unns
-        return unns
-    unns = None if unevts == None else _us()
-    if (verbose):
-        print('nevts  = ', nns)
-        if unns != None:
-            print('unevts = ', unns) 
-
-    # prepare data and mc pdfs
-    if (verbose):
-        print('selection for mc pdfs : ', varnames[:mc_level])
-    sels  = [selection(mc, varnames[:mc_level], varranges[:mc_level]) for mc in mcs]
-    #print('sizes mc : ', [np.sum(sel) for sel in sels])
-    tmcs  = [mc[sel] for mc, sel in zip(mcs, sels)]
-    
-    sel   = selection(data, varnames, varranges)
-    #print('sizes data : ', np.sum(sel))
-    tdata = data[sel]
-
-    res, x, ell, pdfs  = fit_ell(tdata, tmcs, nns, unns, varname, varrange, bins)
-    if (verbose):
-        print('Fit success ? ', res.success)
-        print('Fit result  = ', res.x)
-
-    return (res, x, ell, pdfs, effs)
-
-    
-
-def _ana_experiment(data, mcs, nevts, unevts = None, level_data = 2):
-
-    #level_mc = 1 if selection is None else level_mc
-    #print(level_mc)
-    #vnames, vranges = (varnames[:-1], varranges[:-1]) if selection is None else selection
-
-    vnames   = ['E', 'num_tracks', 'blob2_E']
-    vranges  = [(2.400, 2.650), (1., 1.1), (0.4, np.inf)]
-    level_mc = min(1, level_data) # MC level selction E in range, 1 track
-
-    sels = [ut.selections(mc, vnames, vranges) for mc  in mcs]
-    effs = [ut.efficiency(sel[level_data])     for sel in sels]
-    tmcs = [mc[sel[level_mc]]                  for mc, sel in zip(mcs, sels)]
-    tdat = data[ut.selections(data, vnames, vranges)[level_data]]
-
-    # expected number of events after selection
-    nns = [ni * eff[0] for ni, eff in zip(nevts, effs)]
-    def _unns():
-        us = [uni * eff[0] for uni, eff in zip(unevts, effs)]
-        us[0] = False
-        return us
-    unns = None if unevts == None else _unns()
-    
-    print('nevts  ', nns)
-    print('unevts ', unns)
-    
-    res, x, ell, pdfs  = fit_ell(tdat, tmcs, nns, unns)
-
-    return (res, x, ell, pdfs)
-
-
 
 #----------------
 #
